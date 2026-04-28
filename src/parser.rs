41a8f3561edb1f5b503e62fe1e9f9563a40334fb
TrystanScottLambert <trystanscottlambert@gmail.com>
2026-04-28 23:32:02 +0800

    Adding dependency

--- src/parser.rs
+++ src/parser.rs
@@ -26,29 +26,94 @@ struct Token<'a> {
     grammar: Grammar,
 }
 
-fn sanitize_string(repr: &str) -> String {
-    repr.replace("**", "^")
-}
-
-fn tokenize(repr: &str) -> Vec<String> {
-    // 5+(12/3)*sin(3)
-    let functions = [
-        "sin", "cos", "tan", "arcsin", "arccos", "arctan", "log10", "ln",
-    ];
-    let mut sanitized = sanitize_string(repr);
-    for function in functions {
-        if sanitized.contains(function) {}
-    }
-    let mut grammar = Vec::new();
-    for item in sanitized.split_ascii_whitespace() {
-        if functions.contains(item) {
-            grammar.push(value);
+impl<'a> Token<'a> {
+    fn new(symbol: &'a str) -> Self {
+        if ["+", "-"].contains(&symbol) {
+            Self {
+                symbol,
+                precidence: 1,
+                grammar: Grammar::Operation,
+            }
+        } else if ["/", "*"].contains(&symbol) {
+            Self {
+                symbol,
+                precidence: 2,
+                grammar: Grammar::Operation,
+            }
+        } else if symbol == "^" {
+            Self {
+                symbol,
+                precidence: 3,
+                grammar: Grammar::Operation,
+            }
+        } else if ["(", ")"].contains(&symbol) {
+            Self {
+                symbol,
+                precidence: 5,
+                grammar: Grammar::Operation,
+            }
+        } else if [
+            "sin", "cos", "tan", "arcsin", "arccos", "arctan", "log10", "ln",
+        ]
+        .contains(&symbol)
+        {
+            Self {
+                symbol,
+                precidence: 4,
+                grammar: Grammar::Function,
+            }
+        } else {
+            Self {
+                symbol,
+                precidence: 0,
+                grammar: Grammar::Number,
+            }
         }
     }
 }
 
-fn build_rpn(tokens: Vec<Token>) {
-    todo!()
+fn sanitize_string(repr: &str) -> String {
+    let mut sanitized = repr.to_string();
+    sanitized = sanitized.replace("**", "^");
+    sanitized = sanitized.replace("arcsin", "ARCSIN");
+    sanitized = sanitized.replace("arccos", "ARCCOS");
+    sanitized = sanitized.replace("arctan", "ARCTAN");
+    sanitized.split_ascii_whitespace().collect()
+}
+
+fn tokenize<'a>(repr: &'a str) -> Vec<Token<'a>> {
+    // 5+(12/3)*sin(3)
+    let mut tokens = Vec::new();
+    let functions = [
+        "sin", "cos", "tan", "ARCSIN", "ARCCOS", "ARCTAN", "log10", "ln",
+    ];
+    let mut sanitized = sanitize_string(repr);
+    for function in functions {
+        if sanitized.contains(function) {
+            sanitized = sanitized.replace(function, &format!(" {} ", function));
+        }
+    }
+
+    let primer = sanitized.split(' ').collect::<Vec<&str>>();
+    for prime in primer {
+        if functions.contains(&prime) {
+            tokens.push(Token::new(&prime.to_lowercase()))
+        } else {
+            for c in prime.chars() {
+                tokens.push(Token::new(&c.to_string()))
+            }
+        }
+    }
+    tokens
+}
+
+fn build_rpn(tokens: Vec<String>) {
+    let functions = [
+        "sin", "cos", "tan", "arcsin", "arccos", "arctan", "log10", "ln",
+    ];
+    let output = Vec::new();
+    let operations = Vec::new();
+    for token in tokens {}
 }
 
 #[cfg(test)]
@@ -56,7 +121,40 @@ mod tests {
     use super::*;
     #[test]
     fn tokenize_brackets() {
-        tokenize("(5+7)*(3/2)")
+        let result = tokenize("(5+7)*(3/2)");
+        dbg!(&result);
+        assert_eq!(
+            result,
+            vec!["(", "5", "+", "7", ")", "*", "(", "3", "/", "2", ")"]
+        );
+    }
+
+    #[test]
+    fn tokenize_functions() {
+        let result = tokenize("sin(5+7)*(cos(3)/2)");
+        dbg!(&result);
+        assert_eq!(
+            result,
+            vec![
+                "sin", "(", "5", "+", "7", ")", "*", "(", "cos", "(", "3", ")", "/", "2", ")"
+            ]
+        );
+    }
+
+    #[test]
+    fn tokenize_nested_functions() {
+        let result = tokenize("sin(cos(arctan(4)))");
+        dbg!(&result);
+        assert_eq!(
+            result,
+            vec!["sin", "(", "cos", "(", "arctan", "(", "4", ")", ")", ")"]
+        );
+    }
+
+    #[test]
+    fn tokenize_exponent() {
+        let result = tokenize("5**6");
+        assert_eq!(result, vec!["5", "^", "6"]);
     }
 
     #[test]
