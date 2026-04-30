11956ced7c476e5fed36e627d330fc4ff9811c86
TrystanScottLambert <trystanscottlambert@gmail.com>
2026-04-30 16:39:11 +0800

    Getting left and right arrow in there.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crossterm::cursor::{self, MoveLeft};
+use crossterm::cursor::{MoveLeft, MoveRight, RestorePosition, SavePosition, position};
 use crossterm::style::Stylize;
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{
@@ -6,97 +6,96 @@ use crossterm::{
     event::{self, Event, KeyCode},
     execute,
     style::Print,
-    terminal::{EnterAlternateScreen, LeaveAlternateScreen, disable_raw_mode, enable_raw_mode},
+    terminal::{disable_raw_mode, enable_raw_mode},
 };
-use yarer::session::{self, Session};
+use yarer::session::Session;
 
 use std::collections::HashMap;
-use std::io::{self, Cursor, Stdout, stdout};
+use std::io::{self, Stdout};
 
 mod parser;
 use crate::parser::sanitize_string;
 
-fn print_in_prompt(stdout: &mut Stdout, counter: i32) {
-    execute!(stdout, Print(format!("In [{counter}]: ").green().bold())).unwrap()
+fn print_in_prompt(stdout: &mut Stdout, counter: i32) -> io::Result<()> {
+    execute!(stdout, Print(format!("In [{counter}]: ").green().bold()))
 }
 
-fn print_out_prompt(stdout: &mut Stdout, counter: i32) {
-    execute!(stdout, Print(format!("Out[{counter}]: ").red().bold())).unwrap()
+fn print_out_prompt(stdout: &mut Stdout, counter: i32) -> io::Result<()> {
+    execute!(stdout, Print(format!("Out[{counter}]: ").red().bold()))
 }
 
-fn print_color_string(stdout: &mut Stdout, buffer_string: String) {
+fn print_color_string(stdout: &mut Stdout, buffer_string: String) -> io::Result<()> {
     for c in buffer_string.chars() {
         if c.is_numeric() || c == '.' {
-            execute!(stdout, Print(format!("{}", c.green()))).unwrap();
+            execute!(stdout, Print(format!("{}", c.green())))?
         } else {
-            execute!(stdout, Print(c)).unwrap();
+            execute!(stdout, Print(c))?
         }
     }
+    Ok(())
 }
 
-fn reset_prompt(stdout: &mut Stdout, counter: i32) {
-    execute!(stdout, Clear(ClearType::CurrentLine)).unwrap();
-    execute!(stdout, Print("\r")).unwrap();
-    print_in_prompt(stdout, counter);
+fn reset_prompt(stdout: &mut Stdout, counter: i32) -> io::Result<()> {
+    execute!(stdout, Clear(ClearType::CurrentLine))?;
+    execute!(stdout, Print("\r"))?;
+    print_in_prompt(stdout, counter)
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let mut stdout = std::io::stdout();
     let mut buffer = String::new();
-    let start_cursor_position: usize = 5;
-    let mut cursor_position: usize = start_cursor;
     let mut history = HashMap::new();
     let mut history_counter = 0;
-    enable_raw_mode().unwrap();
+    let start_line_position = 8;
+    enable_raw_mode()?;
     let mut counter = 1;
-    execute!(stdout, Clear(ClearType::All)).unwrap();
-    execute!(stdout, Print("\n\r")).unwrap();
-
-    print_in_prompt(&mut stdout, counter);
+    execute!(stdout, Clear(ClearType::All))?;
+    execute!(stdout, Print("\n\r"))?;
+    print_in_prompt(&mut stdout, counter)?;
+    execute!(stdout, SavePosition)?;
     loop {
-        let thing = event::read().unwrap();
+        let thing = event::read()?;
         match thing {
             Event::Key(key_event) => match key_event.code {
                 KeyCode::Esc => break,
                 KeyCode::Char(c) => {
                     if c.is_numeric() || c == '.' {
-                        execute!(stdout, Print(format!("{}", c.green()))).unwrap();
+                        execute!(stdout, Print(format!("{}", c.green())))?;
                     } else {
-                        execute!(stdout, Print(c)).unwrap();
+                        execute!(stdout, Print(c))?;
                     }
-                    buffer.insert(cursor_position, c);
-                    cursor_position += 1;
+                    buffer.push(c);
                 }
                 KeyCode::Enter => {
-                    execute!(stdout, Print("\r\n")).unwrap();
+                    execute!(stdout, Print("\r\n"))?;
 
                     if buffer.trim().is_empty() {
-                        execute!(stdout, Print("\r\n")).unwrap();
-                        print_in_prompt(&mut stdout, counter);
+                        execute!(stdout, Print("\r\n"))?;
+                        print_in_prompt(&mut stdout, counter)?;
                     } else if buffer == "clear" {
-                        execute!(stdout, Clear(ClearType::All)).unwrap();
-                        print_in_prompt(&mut stdout, counter);
+                        execute!(stdout, Clear(ClearType::All))?;
+                        print_in_prompt(&mut stdout, counter)?;
                         buffer.clear();
                     } else {
-                        print_out_prompt(&mut stdout, counter);
+                        print_out_prompt(&mut stdout, counter)?;
                         let session = Session::init();
                         let sanitized_string = sanitize_string(&buffer);
                         let mut resolver = session.process(&sanitized_string);
                         // session.set("x", 1) // we can add variables in the future
                         let answer = resolver.resolve();
                         if let Ok(num) = answer {
-                            execute!(stdout, Print(format!("{}", num))).unwrap();
+                            execute!(stdout, Print(format!("{}", num)))?;
                         } else {
-                            execute!(stdout, Print("\n\rError reading. Try again.")).unwrap();
+                            execute!(stdout, Print("\n\rError reading. Try again."))?;
                         }
                         history.insert(counter, buffer.clone());
                         history_counter = 0;
 
                         buffer.clear();
-                        execute!(stdout, Print("\r\n")).unwrap();
-                        execute!(stdout, Print("\r\n")).unwrap();
+                        execute!(stdout, Print("\r\n"))?;
+                        execute!(stdout, Print("\r\n"))?;
                         counter += 1;
-                        print_in_prompt(&mut stdout, counter);
+                        print_in_prompt(&mut stdout, counter)?;
                     }
                 }
                 KeyCode::Up => {
@@ -106,8 +105,8 @@ fn main() {
                         history_counter += 1;
                         let index = counter - history_counter;
                         buffer = history.get(&(index)).unwrap().to_string();
-                        reset_prompt(&mut stdout, counter);
-                        print_color_string(&mut stdout, buffer.clone());
+                        reset_prompt(&mut stdout, counter)?;
+                        print_color_string(&mut stdout, buffer.clone())?;
                     }
                 }
                 KeyCode::Down => {
@@ -115,30 +114,37 @@ fn main() {
                         history_counter -= 1;
                         let index = counter - history_counter;
                         buffer = history.get(&(index)).unwrap().to_string();
-                        reset_prompt(&mut stdout, counter);
-                        print_color_string(&mut stdout, buffer.clone());
+                        reset_prompt(&mut stdout, counter)?;
+                        print_color_string(&mut stdout, buffer.clone())?;
                     } else {
                         history_counter = 0;
                         buffer.clear();
-                        reset_prompt(&mut stdout, counter);
-                        execute!(stdout, Print("")).unwrap();
+                        reset_prompt(&mut stdout, counter)?;
+                        execute!(stdout, Print(""))?;
                     }
                 }
                 KeyCode::Backspace => {
                     buffer.pop();
-                    reset_prompt(&mut stdout, counter);
-                    print_color_string(&mut stdout, buffer.clone());
+                    reset_prompt(&mut stdout, counter)?;
+                    print_color_string(&mut stdout, buffer.clone())?;
                 }
-                KeyCode::Home => {
-                    let (_, current_column) = cursor::position().unwrap();
-                    let offset = current_column - start_cursor_position;
-
-                    execute!(stdout, Cursor(MoveLeft(offset))).unwrap();
+                KeyCode::Left => {
+                    let (column, _) = position()?;
+                    if column > start_line_position {
+                        execute!(stdout, MoveLeft(1), SavePosition)?;
+                    }
+                }
+                KeyCode::Right => {
+                    let (column, _) = position()?;
+                    if column < (buffer.len() as u16) + start_line_position {
+                        execute!(stdout, MoveRight(1), SavePosition)?;
+                    }
                 }
                 _ => {}
             },
             _ => todo!(),
         }
     }
-    disable_raw_mode().unwrap();
+    disable_raw_mode()?;
+    Ok(())
 }
